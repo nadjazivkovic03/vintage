2fdbf0f3b2b12d7e835434b68275ff3a23fcd680
Nadja Zivkovic <zivkovic.nadja03@gmail.com>
2026-02-02 00:45:40 +0100

    lang1

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { LanguageProvider } from "./LanguageContext";
-//import "./global.css";
+import "styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
